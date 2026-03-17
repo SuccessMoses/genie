ae24d91834fb176d56272c0a81ef3a2a672bd110
Success Moses <msucc2024@gmail.com>
2026-03-17 14:30:07 +0100

    rewrite parser

--- app/Main.hs
+++ app/Main.hs
@@ -7,86 +7,46 @@ import qualified Text.Megaparsec            as Parsec
 import qualified Text.Megaparsec.Char       as CharParser
 import qualified Text.Megaparsec.Char.Lexer as Lexer
 import           Control.Monad.Combinators.Expr
-
-{-
-  WHILE Language Grammar
-  a   ::= x | n | - a | a opa a
-  b   ::= true | false | not b | b opb b | a opr a
-  opa ::= + | - | * | /
-  opb ::= and | or
-  opr ::= > | <
-  S   ::= x := a | skip | S1; S2 | ( S ) | if b then S1 else S2 | while b do S
--}
-
-data ArithExpr = 
-    Variable String
-    | Num Int
-    | Neg ArithExpr
-    | Opa Opa ArithExpr ArithExpr
-
-data BoolExpr = 
-    True
-    | False
-    | Not BoolExpr
-    | Opb Opb BoolExpr BoolExpr
-    | Opr Opr ArithExpr ArithExpr
-
-data Opa = Add | Sub | Mul | Div
-
-data Opb = And | Or
-
-data Opr = LT | GT
-
-data Stmt =
-    Assign String ArithExpr
-    | Skip
-    | Seq [Stmt]
-    | If BoolExpr Stmt Stmt
-    | While BoolExpr Stmt
+import qualified Control.Monad.Combinators.Expr as ExprParser
 
 
 type Parser = Parsec.Parsec Void String
 
-space :: Parser ()
-space = Lexer.space CharParser.space1 lineComment blockComment
-  where
-    lineComment = Lexer.skipLineComment "//"
-    blockComment = Lexer.skipBlockComment "/*" "*/"
+data Expr =
+    Num Int
+    | Add Expr Expr
+    | Mul Expr Expr
+    | Neg Expr
+    deriving (Show)
 
-symbol :: String -> Parser String
-symbol = Lexer.symbol space
+space :: Parser ()
+space = Lexer.space CharParser.space1 Parsec.empty Parsec.empty
 
 lexeme :: Parser a -> Parser a
 lexeme = Lexer.lexeme space
 
-reserved :: String -> Parser ()
-reserved w = 
-    lexeme $ Parsec.try $ 
-        CharParser.string w *> Parsec.notFollowedBy CharParser.alphaNumChar
+symbol :: String -> Parser String
+symbol = Lexer.symbol space
 
-reservedWords :: [String]
-reservedWords = 
+numLiteral :: Parser Expr
+numLiteral = lexeme $ Num <$> Lexer.decimal
+
+parens :: Parser a -> Parser a
+parens = Parsec.between (symbol "(") (symbol ")")
+
+termParser :: Parser Expr
+termParser = parens expr <|> numLiteral
+
+expr :: Parser Expr
+expr = ExprParser.makeExprParser termParser termOperators
+
+termOperators :: [[Operator Parser Expr]]
+termOperators =
     [
-        "if",
-        "then",
-        "else",
-        "while",
-        "do",
-        "skip",
-        "true",
-        "false",
-        "not",
-        "and",
-        "or"
+        [ExprParser.Prefix (Neg <$ symbol "-")],
+        [ExprParser.InfixL (Add <$ symbol "+")],
+        [ExprParser.InfixL (Mul <$ symbol "*")]
     ]
-
-identifier :: Parser String
-identifier = lexeme $ Parsec.try $ do
-    x <- (:) <$> CharParser.letterChar <*> Parsec.many CharParser.alphaNumChar
-    if x `elem` reservedWords
-    then fail $ "keyword " ++ show x ++ " cannot be an identifier"
-    else return x
-
 
 -- so haskell does not complain
 main :: IO ()
