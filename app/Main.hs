db1952bad12eed36ce161f902fff482c7ef7e305
Success Moses <msucc2024@gmail.com>
2026-03-17 15:39:42 +0100

    add term parser

--- app/Main.hs
+++ app/Main.hs
@@ -9,15 +9,40 @@ import qualified Text.Megaparsec.Char.Lexer as Lexer
 import           Control.Monad.Combinators.Expr
 import qualified Control.Monad.Combinators.Expr as ExprParser
 
+{-
+  ARITH Language Grammar
+  a ::= 0 | a++ | a opa a | if b then a else a
+  b ::= true | false | not b | b opb b | a opr a
+  t   ::=  a | b
+  opa ::= + | - | *
+  opb ::= and | or
+  opr ::= > | <
+  S   ::= x := t | skip
+  Seq ::= null | S; Seq
+-}
 
 type Parser = Parsec.Parsec Void String
 
-data Expr =
-    Num Int
-    | Add Expr Expr
-    | Mul Expr Expr
-    | Neg Expr
+data AExpr =
+    ZeroE
+    | SuccE AExpr
+    | OpEA OpA AExpr AExpr
+    | IfE BExpr AExpr AExpr
     deriving (Show)
+
+data BExpr =
+    TrueE
+    | FalseE
+    | Not BExpr
+    | OpEB OpB BExpr BExpr
+    | OpER OpR AExpr AExpr
+    deriving (Show)
+
+data OpA = AddE | SubE | MulE deriving (Show)
+
+data OpR = LTE | GTE deriving (Show)
+
+data OpB = AndE | OrE deriving (Show)
 
 space :: Parser ()
 space = Lexer.space CharParser.space1 Parsec.empty Parsec.empty
@@ -28,25 +53,53 @@ lexeme = Lexer.lexeme space
 symbol :: String -> Parser String
 symbol = Lexer.symbol space
 
-numLiteral :: Parser Expr
-numLiteral = lexeme $ Num <$> Lexer.decimal
-
 parens :: Parser a -> Parser a
 parens = Parsec.between (symbol "(") (symbol ")")
 
-termParser :: Parser Expr
-termParser = parens expr <|> numLiteral
+aLit :: Parser AExpr
+aLit = lexeme $ do
+    n <- (Lexer.decimal :: Parser Int)
+    return $ foldr (\_ acc -> SuccE acc) ZeroE [1..n]
 
-expr :: Parser Expr
-expr = ExprParser.makeExprParser termParser termOperators
+ifParser :: Parser AExpr
+ifParser = Parsec.try $ do
+    _ <- symbol "if"
+    b <- bExpr
+    _ <- symbol "then"
+    a1 <- aExpr
+    _ <- symbol "else"
+    IfE b a1 <$> aExpr
 
-termOperators :: [[Operator Parser Expr]]
-termOperators =
-    [
-        [ExprParser.Prefix (Neg <$ symbol "-")],
-        [ExprParser.InfixL (Add <$ symbol "+")],
-        [ExprParser.InfixL (Mul <$ symbol "*")]
-    ]
+aExprTerm :: Parser AExpr
+aExprTerm = 
+    aLit
+    <|> ifParser
+    <|> parens aExpr
+
+aExpr :: Parser AExpr
+aExpr = ExprParser.makeExprParser aExprTerm [[]]
+
+bExpr :: Parser BExpr
+bExpr = ExprParser.makeExprParser bExprTerm [[]]
+    where
+        bExprTerm = 
+            TrueE <$ symbol "true"
+            <|> FalseE <$ symbol "false"
+            <|> parens bExpr
+
+term :: Parser (Either AExpr BExpr)
+term = (Left <$> aExpr) <|> (Right <$> bExpr)
+
+-- expr :: Parser Expr
+-- expr = ExprParser.makeExprParser termParser termOperators
+
+-- termOperators :: [[Operator Parser Expr]]
+-- termOperators =
+--     [
+--         [ExprParser.Prefix (Neg <$ symbol "-")],
+--         [ExprParser.InfixL (Add <$ symbol "+")],
+--         [ExprParser.InfixL (Mul <$ symbol "*")]
+--     ]
 
 -- so haskell does not complain
 main :: IO ()
